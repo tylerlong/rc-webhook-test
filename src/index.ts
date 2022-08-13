f1010c2ab87a93fdbad18cf55d8fe8f7c1ae3c94
Tyler Long <tyler4long@gmail.com>
2022-08-12 17:09:45 -0700

    PubNub

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import RingCentral from '@rc-ex/core';
+import PubNubExtension from '@rc-ex/pubnub';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
@@ -7,13 +8,22 @@ const rc = new RingCentral({
 });
 
 const main = async () => {
-  await rc.authorize({
-    username: process.env.RINGCENTRAL_USERNAME!,
-    extension: process.env.RINGCENTRAL_EXTENSION,
-    password: process.env.RINGCENTRAL_PASSWORD!,
-  });
+  rc.token = {access_token: process.env.RINGCENTRAL_TOKEN};
   console.log(rc.token?.access_token);
-  await rc.revoke();
+  const extInfo = await rc.restapi().account().extension().get();
+  console.log(JSON.stringify(extInfo, null, 2));
+
+  const pubNubExtension = new PubNubExtension();
+  await rc.installExtension(pubNubExtension);
+  const subInfo = await pubNubExtension.subscribe(
+    [
+      '/restapi/v1.0/account/~/telephony/sessions?direction=Inbound&missedCall=true',
+    ],
+    event => {
+      console.log(JSON.stringify(event, null, 2));
+    }
+  );
+  console.log(JSON.stringify(subInfo._subscriptionInfo, null, 2));
 };
 
 main();
